bea864465597affd83ffa7cc5919446dbd90f9a5
靓仔 <970767662@qq.com>
2020-03-11 15:40:40 +0800

    商城首页完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,62 +5,51 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    swiperList:[],
+    navList:[],
+    floorList:[]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+   this.getSwiperList()
+   this.getNavList()
+   this.getFloorList()
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  // 获取轮播图数据
+  getSwiperList() {
+    wx.request({
+      url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
+      success: (result) => {
+        this.setData({
+          swiperList:result.data.message
+        })
+      }
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  // 获取导航栏数据
+  getNavList() {
+    wx.request({
+      url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/catitems',
+      success: (result) => {
+        this.setData({
+          navList:result.data.message
+        })
+      }
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 获取楼层数据
+  getFloorList() {
+    wx.request({
+      url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/floordata',
+      success: (result) => {
+        this.setData({
+          floorList:result.data.message
+        })
+      }
+    });
   }
+
 })

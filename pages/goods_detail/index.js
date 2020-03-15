e4025c01736d366f2a5908c55a7312a1364a2802
靓仔 <970767662@qq.com>
2020-03-15 19:53:25 +0800

    商品详情页完成

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -1,66 +1,72 @@
-// pages/goods_detail/index.js
+import {request} from '../../request/index'
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    goods_id:0,
+    goodsData:{}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    const {goods_id} = options
+    this.setData({
+      goods_id
+    })
+    this.getDetailData()
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  getDetailData() {
+    request({
+      url:'/goods/detail',
+      data:{
+        goods_id:this.data.goods_id
+      }
+    }).then(res => {
+      this.setData({
+        goodsData:res.data.message
+      })
+      
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  handleTap(event) {
+    const urls = this.data.goodsData.pics.map(v => v.pics_mid_url)
+    const {current} = event.currentTarget.dataset
+    wx.previewImage({
+      current,
+      urls
+    }); 
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleAddGoods() {
+    
+    let cartData = wx.getStorageSync('cart') || [];
+    let index = cartData.findIndex(v => {
+      return v.goods_id === this.data.goodsData.goods_id
+    });
+    
+    
+    if (index === -1) {
+      const {goods_id,goods_name,goods_price,goods_small_logo} = this.data.goodsData
+      cartData.push({
+        goods_id,
+        goods_name,
+        goods_price,
+        goods_number:1,
+        goods_image:goods_small_logo
+      })
+    } else {
+      cartData[index].goods_number++
+    }
+    wx.setStorageSync('cart',cartData)
+    wx.showToast({
+      title: '加入购物车成功',
+      icon: 'success'
+    });
+      
   }
+
+  
 })
